d686d6ebaaa00d0ee58916bf5af127df765241d2
Yulia <y-bushmanova@northwestern.edu>
2011-02-15 16:25:57 -0600

    failing on persistence token

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,55 @@
+class UserSessionsController < ApplicationController
+  before_filter :login_required, :only => [:admin]
+  helper :all # include all helpers, all the time
+  include UserSessionsHelper
+  
+  def new
+    @user_session = UserSession.new
+  end
+  
+  # Display admin login form
+  def admin
+  end
+  
+  def create
+    flash[:notice] =''
+    @user_session = UserSession.new(params[:user_session]);
+    has_valid_email = valid_email?(params[:user_session][:email])
+    has_valid_password = valid_password?(params[:user_session][:password])
+    
+    @user = get_user(params[:user_session])
+    valid_user = ! @user.nil?
+    if valid_user and has_valid_password
+      @user_session = UserSession.create(@user) 
+    end
+    @user_session.save do |result|
+      if result
+        flash[:notice] = "Successfully logged in."
+        logger.info "Successfully logged in user #{params[:user_session][:email]}."
+        if is_admin?
+          flash[:notice] = "Logged in as admin"
+          redirect_to admin_url
+        else
+          redirect_to root_url
+        end
+      elsif ! valid_user and ! has_valid_email
+         @user_session.destroy
+         flash[:notice] = "Please enter a proper email and password."
+         render :action => :new
+      elsif has_valid_email and has_valid_password
+        redirect_to(new_user_path(:email => params[:user_session][:email]))
+      else
+        flash[:notice] = "Please enter a proper password."
+         @user_session.destroy
+         render :action => :new
+       end
+    end
+  end
+  
+  def destroy
+    @user_session = UserSession.find
+    @user_session.destroy
+    flash[:notice] = "Successfully logged out."
+    redirect_to login_url
+  end
+end
