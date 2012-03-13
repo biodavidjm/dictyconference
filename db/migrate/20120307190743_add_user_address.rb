e7e8efa1124341d4a2d7d6600c8882345f697095
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-03-13 13:22:12 -0500

    fixed the issue with duplicate user field

--- db/migrate/20120307190743_add_user_address.rb
+++
@@ -1,8 +0,0 @@
-class AddUserAddress < ActiveRecord::Migration
-  def up
-    add_column :users, :address, :string
-  end
-
-  def down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20120312204951) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                                 :null => false
+    t.string   "email"
     t.boolean  "is_admin",           :default => false
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
