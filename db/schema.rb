3226d167486584d729a7ae3ec3dd6c3f53672851
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2012-03-13 11:54:40 -0500

    Is Registered migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307190743) do
+ActiveRecord::Schema.define(:version => 20120312204951) do
 
   create_table "abstracts", :force => true do |t|
     t.integer  "user_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20120307190743) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
+    t.string   "email",                                 :null => false
     t.boolean  "is_admin",           :default => false
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20120307190743) do
     t.string   "phone"
     t.string   "institute"
     t.string   "address"
+    t.boolean  "is_registered"
   end
 
 end
