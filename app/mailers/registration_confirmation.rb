47a35af4c71879afa80bb8a2081865dcfd06c12b
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2012-03-15 17:30:41 -0500

    Added a mailer to send confirmation emails

---
+++ app/mailers/registration_confirmation.rb
@@ -0,0 +1,14 @@
+class RegistrationConfirmation < ActionMailer::Base
+  default from: "dictybase@northwestern.edu"
+  
+  def registration_confirmation_to_user (user)
+    @user = user
+    mail(:to => user.email, 
+      :from => "dictyBase <dictybase@northwestern.edu>"
+    	:subject => "Dicty12 Registration Confirmation",
+    	:body => {:user => user}
+    	sent_on => Time.now
+    	)
+  end
+
+end
