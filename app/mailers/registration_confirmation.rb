f2d3cc301d3e08d2481ba8d685fd8686ff89798c
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2012-03-16 13:03:15 -0500

    Added 2 methods for #7; not working :bomb:

--- app/mailers/registration_confirmation.rb
+++ app/mailers/registration_confirmation.rb
@@ -3,12 +3,26 @@ class RegistrationConfirmation < ActionMailer::Base
   
   def registration_confirmation_to_user (user)
     @user = user
-    mail(:to => user.email, 
-      :from => "dictyBase <dictybase@northwestern.edu>"
-    	:subject => "Dicty12 Registration Confirmation",
-    	:body => {:user => user},
-    	sent_on => Time.now
-    	)
+        mail(:to => user.email, 
+          :from => "dictybase@northwestern.edu",
+          :subject => "Dicty12 Registration Confirmation",
+          :body => {:user => user}
+          )
+    # to  user.email
+    #     from  "dictyBase <dictybase@northwestern.edu>"
+    #     subject "Dicty12 Registration Confirmation"
+    #     sent_on Time.now
+    #     body  :user => user
   end
+  
+  # def registration_confirmation_to_host (user)
+  #     @user = user
+  #     mail(:to => "", 
+  #       :from => "dictyBase <dictybase@northwestern.edu>",
+  #       :subject => "New Dicty12 Registration",
+  #       :body => {:user => user},
+  #       sent_on => Time.now
+  #       )
+  #   end
 
 end
