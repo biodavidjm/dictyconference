b1dd09066b232e83e7ccd8bc070beab04f2bb002
Yogesh Pandit <yogeshp08@gmail.com>
2012-03-26 22:09:42 -0500

    Added currency converter EUR->USD :star2:

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Dicty11
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails application jquery-ui-datepicker)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails application jquery-ui-datepicker money-min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
