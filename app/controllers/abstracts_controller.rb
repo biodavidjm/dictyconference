2bba7e7b6c95ddbb95e0a4f285cd54c694339cb2
Yogesh Pandit <yogeshp08@gmail.com>
2013-05-30 17:07:47 -0500

    User should be logged_in to view details

--- app/controllers/abstracts_controller.rb
+++ app/controllers/abstracts_controller.rb
@@ -1,122 +1,128 @@
 class AbstractsController < ApplicationController
-  skip_before_filter :login_required, :only => [ :index, :show ]
 
-  # GET /abstracts
-  # GET /abstracts.xml
-  def index
-	  session[:where_from] = 'abstract'
-	  if params[:user_id]
-      @abstracts = Abstract.find(:all, :conditions =>['user_id = :user_id',
-        {:user_id => params[:user_id] } ])
-    else
-      @abstracts = Abstract.all
-    end
+	skip_before_filter :login_required, :only => [ :index, :show ]
+	include UsersHelper
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @abstracts }
-      format.csv {
+	# GET /abstracts
+	# GET /abstracts.xml
+	def index
+		session[:where_from] = 'abstract'
+		if params[:user_id]
+			@abstracts = Abstract.find(:all, :conditions =>['user_id = :user_id',
+												   {:user_id => params[:user_id] } ])
+		else
+			@abstracts = Abstract.all
+		end
 
-        buffer = FasterCSV.generate do |csv|
-          fields = [
-            :user_id,
-            :title,
-            :authors,
-            :address,
-            :presenter,
-            :abstract_type,
-            :abstract,
-            :agreement,
-            :note_to_organizers,
-            :created_at,
-            :updated_at
-          ]
-          csv << fields.map{|field| field.to_s}
-          for abstract in @abstracts
-            csv << fields.map{|field| value = abstract.send(field)}
-          end
-        end
-        send_data( buffer, :type => 'text/csv; charset=iso-8859-1; header=present', :filename => 'abstracts.csv')
-      }
-    end
-  end
+		respond_to do |format|
+			format.html # index.html.erb
+			format.xml  { render :xml => @abstracts }
+			format.csv {
 
-  # GET /abstracts/1
-  # GET /abstracts/1.xml
-  def show
-    @abstract = Abstract.find(params[:id])
-	 #@abstract = Abstract.where(:user_id => current_user.id).find(params[:id])
+				buffer = FasterCSV.generate do |csv|
+					fields = [
+						:user_id,
+						:title,
+						:authors,
+						:address,
+						:presenter,
+						:abstract_type,
+						:abstract,
+						:agreement,
+						:note_to_organizers,
+						:created_at,
+						:updated_at
+					]
+					csv << fields.map{|field| field.to_s}
+					for abstract in @abstracts
+						csv << fields.map{|field| value = abstract.send(field)}
+					end
+				end
+				send_data( buffer, :type => 'text/csv; charset=iso-8859-1; header=present', :filename => 'abstracts.csv')
+			}
+		end
+	end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @abstract }
-    end
-  end
+	# GET /abstracts/1
+	# GET /abstracts/1.xml
+	def show
+		if logged_in?
+			@abstract = Abstract.find(params[:id])
+			#@abstract = Abstract.where(:user_id => current_user.id).find(params[:id])
 
-  # GET /abstracts/new
-  # GET /abstracts/new.xml
-  def new
-    @abstract = Abstract.new( :user_id=>current_user.id )
+			respond_to do |format|
+				format.html # show.html.erb
+				format.xml  { render :xml => @abstract }
+			end
+		else
+			redirect_to login_path
+		end
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @abstract }
-    end
-  end
+	# GET /abstracts/new
+	# GET /abstracts/new.xml
+	def new
+		@abstract = Abstract.new( :user_id=>current_user.id )
 
-  # GET /abstracts/1/edit
-  def edit
-    @abstract = Abstract.find(params[:id])
-  end
+		respond_to do |format|
+			format.html # new.html.erb
+			format.xml  { render :xml => @abstract }
+		end
+	end
 
-  # POST /abstracts
-  # POST /abstracts.xml
-  def create
-    @abstract = Abstract.new(params[:abstract])
+	# GET /abstracts/1/edit
+	def edit
+		@abstract = Abstract.find(params[:id])
+	end
 
-    respond_to do |format|
-      if @abstract.save
-      	logger.info "Sending confirmation email for abstract submission to #{User.find(@abstract.user_id)}"
+	# POST /abstracts
+	# POST /abstracts.xml
+	def create
+		@abstract = Abstract.new(params[:abstract])
 
-      	# Send confirmation emails to user and the host
-      	RegistrationConfirmation.abstract_confirmation_to_user(@abstract).deliver
+		respond_to do |format|
+			if @abstract.save
+				logger.info "Sending confirmation email for abstract submission to #{User.find(@abstract.user_id)}"
 
-    	logger.info "Abstract submission successful"
+				# Send confirmation emails to user and the host
+				RegistrationConfirmation.abstract_confirmation_to_user(@abstract).deliver
 
-        format.html { redirect_to(@abstract, :notice => 'Abstract was successfully created.') }
-        format.xml  { render :xml => @abstract, :status => :created, :location => @abstract }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @abstract.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+				logger.info "Abstract submission successful"
 
-  # PUT /abstracts/1
-  # PUT /abstracts/1.xml
-  def update
-    @abstract = Abstract.find(params[:id])
+				format.html { redirect_to(@abstract, :notice => 'Abstract was successfully created.') }
+				format.xml  { render :xml => @abstract, :status => :created, :location => @abstract }
+			else
+				format.html { render :action => "new" }
+				format.xml  { render :xml => @abstract.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
 
-    respond_to do |format|
-      if @abstract.update_attributes(params[:abstract])
-        format.html { redirect_to(@abstract, :notice => 'Abstract was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @abstract.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+	# PUT /abstracts/1
+	# PUT /abstracts/1.xml
+	def update
+		@abstract = Abstract.find(params[:id])
 
-  # DELETE /abstracts/1
-  # DELETE /abstracts/1.xml
-  def destroy
-    @abstract = Abstract.find(params[:id])
-    @abstract.destroy
+		respond_to do |format|
+			if @abstract.update_attributes(params[:abstract])
+				format.html { redirect_to(@abstract, :notice => 'Abstract was successfully updated.') }
+				format.xml  { head :ok }
+			else
+				format.html { render :action => "edit" }
+				format.xml  { render :xml => @abstract.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
 
-    respond_to do |format|
-      format.html { redirect_to(abstracts_url) }
-      format.xml  { head :ok }
-    end
-  end
+	# DELETE /abstracts/1
+	# DELETE /abstracts/1.xml
+	def destroy
+		@abstract = Abstract.find(params[:id])
+		@abstract.destroy
+
+		respond_to do |format|
+			format.html { redirect_to(abstracts_url) }
+			format.xml  { head :ok }
+		end
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,141 +1,152 @@
 class UsersController < ApplicationController
 
-  skip_before_filter :login_required, :only => [:new,:create]
+	skip_before_filter :login_required, :only => [:new,:create]
+	include UsersHelper
 
-  # GET /users
-  # GET /users.xml
-  def index
-    @users = User.all
+	# GET /users
+	# GET /users.xml
+	def index
+		if logged_in?
+			if is_admin?
+				@users = User.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-      format.csv {
-        buffer = FasterCSV.generate do |csv|
-          fields_user = [
-            :id,
-            :email,
-            :first_name,
-            :last_name,
-          ]
-          all_fields = fields_user.map{|field| field.to_s}
-          csv << all_fields
-          for user in @users
-            all_data = fields_user.map{|field| value = user.send(field)}
-           csv << all_data
-          end
-        end
-        send_data( buffer, :type => 'text/csv; charset=iso-8859-1; header=present', :filename => 'users.csv')
-
-      }
-    end
-  end
-
-  # GET /users/1
-  # GET /users/1.xml
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.xml
-  def new
-    email = params[:user_session][:email] if ! params[:user_session].nil?
-    email ||= params[:email]
-    password = params[:user_session][:password] if ! params[:user_session].nil?
-    @user = User.new(:password=>password, :email=>email )
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.xml
-  def create
-    @user = User.new(params[:user])
-    @user.is_registered = 0
-     respond_to do |format|
-      if @user.save
-          logger.info "Successfully created profile."
-          flash[:notice] = "Successfully created profile."
-          if session[:where_from] == 'registration'
-            format.html { redirect_to(new_registration_path(@user.id)) }
-            # format.xml  { render :xml => @user, :status => :created, :location => @user }
-		  elsif session[:where_from] == 'abstract'
-            format.html { redirect_to(abstract_path) }
-            # format.xml  { render :xml => @user, :status => :created, :location => @user }
-          end
-          format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        flash[:notice] = @user.errors
-        if session[:where_from] == 'registration'
-          # flash[:notice] = @user.errors
-          format.html { redirect_to new_registration_path }
-          # format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-        else
-          # flash[:notice] = @user.errors
-          format.html { render :action => 'new' }
-          # format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-        end
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.xml
-  def update
-    @user = User.find(params[:id])
-    @user.attributes = params[:user]
-    if session[:where_from] == 'registration'
-		@user.is_registered = 1
+				respond_to do |format|
+					format.html # index.html.erb
+					format.xml  { render :xml => @users }
+					format.csv {
+						buffer = FasterCSV.generate do |csv|
+							fields_user = [
+								:id,
+								:email,
+								:first_name,
+								:last_name,
+							]
+							all_fields = fields_user.map{|field| field.to_s}
+							csv << all_fields
+							for user in @users
+								all_data = fields_user.map{|field| value = user.send(field)}
+								csv << all_data
+							end
+						end
+						send_data( buffer, :type => 'text/csv; charset=iso-8859-1; header=present', :filename => 'users.csv')
+					}
+				end
+			else
+				flash[:notice] = "You need to be an ADMIN to view list of users"
+			end
+		else
+			redirect_to login_path
+		end
 	end
 
-    if params[:commit] != 'Cancel'
-      respond_to do |format|
-        if @user.save
-          flash[:notice] = "Successfully updated registration"
-          logger.info "Successfully updated registration for #{@user.email}"
+	# GET /users/1
+	# GET /users/1.xml
+	def show
+		if logged_in?
+			@user = User.find(params[:id])
+			respond_to do |format|
+				format.html # show.html.erb
+				format.xml  { render :xml => @user }
+			end
+		else
+			redirect_to login_path
+		end 
+	end
 
-          # Send confirmation emails to user and the host
-          RegistrationConfirmation.update_confirmation_to_host(@user).deliver
-          RegistrationConfirmation.update_confirmation_to_user(@user).deliver
+	# GET /users/new
+	# GET /users/new.xml
+	def new
+		email = params[:user_session][:email] if ! params[:user_session].nil?
+		email ||= params[:email]
+		password = params[:user_session][:password] if ! params[:user_session].nil?
+		@user = User.new(:password=>password, :email=>email )
 
-          format.html { redirect_to registration_path(current_user) }
-          format.xml  { render :xml => @user, :status => :created, :location => @user }
-        else
-          logger.info "Unable to update registration."
-          format.html { render :action => "edit" }
-          format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-        end
-      end
-    else
-      flash[:notice] = "Cancelled"
-      redirect_to registration_path(current_user)
-    end
-  end
+		respond_to do |format|
+			format.html # new.html.erb
+			format.xml  { render :xml => @user }
+		end
+	end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+	# GET /users/1/edit
+	def edit
+		@user = User.find(params[:id])
+	end
 
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
-    end
-  end
+	# POST /users
+	# POST /users.xml
+	def create
+		@user = User.new(params[:user])
+		@user.is_registered = 0
+		respond_to do |format|
+			if @user.save
+				logger.info "Successfully created profile."
+				flash[:notice] = "Successfully created profile."
+				if session[:where_from] == 'registration'
+					format.html { redirect_to(new_registration_path(@user.id)) }
+					# format.xml  { render :xml => @user, :status => :created, :location => @user }
+				elsif session[:where_from] == 'abstract'
+					format.html { redirect_to(abstract_path) }
+					# format.xml  { render :xml => @user, :status => :created, :location => @user }
+				end
+				format.xml  { render :xml => @user, :status => :created, :location => @user }
+			else
+				flash[:notice] = @user.errors
+				if session[:where_from] == 'registration'
+					# flash[:notice] = @user.errors
+					format.html { redirect_to new_registration_path }
+					# format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+				else
+					# flash[:notice] = @user.errors
+					format.html { render :action => 'new' }
+					# format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+				end
+				format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
+
+	# PUT /users/1
+	# PUT /users/1.xml
+	def update
+		@user = User.find(params[:id])
+		@user.attributes = params[:user]
+		if session[:where_from] == 'registration'
+			@user.is_registered = 1
+		end
+
+		if params[:commit] != 'Cancel'
+			respond_to do |format|
+				if @user.save
+					flash[:notice] = "Successfully updated registration"
+					logger.info "Successfully updated registration for #{@user.email}"
+
+					# Send confirmation emails to user and the host
+					RegistrationConfirmation.update_confirmation_to_host(@user).deliver
+					RegistrationConfirmation.update_confirmation_to_user(@user).deliver
+
+					format.html { redirect_to registration_path(current_user) }
+					format.xml  { render :xml => @user, :status => :created, :location => @user }
+				else
+					logger.info "Unable to update registration."
+					format.html { render :action => "edit" }
+					format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+				end
+			end
+		else
+			flash[:notice] = "Cancelled"
+			redirect_to registration_path(current_user)
+		end
+	end
+
+	# DELETE /users/1
+	# DELETE /users/1.xml
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		respond_to do |format|
+			format.html { redirect_to(users_url) }
+			format.xml  { head :ok }
+		end
+	end
 end
