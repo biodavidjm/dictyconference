853c194622c1b261b0d864e5b4eb8989bf8458fa
Yogesh Pandit <yogeshp08@gmail.com>
2013-06-04 16:40:57 -0500

    Broken test for abstracts controller

--- spec/controllers/abstracts_controller_spec.rb
+++ spec/controllers/abstracts_controller_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
+require 'pp'
 
 describe AbstractsController do
 
-	fixtures :abstracts
+	fixtures :abstracts, :users
+
+	before(:each) do
+		activate_authlogic
+	end
 
 	it "should create an abstract" do
+		UserSession.create(users(:one))
+		user_session = UserSession.find
 		assert_difference 'Abstract.count' do
-			post :create, abstract: {title: "Abstract 2 for Dicty Conference", authors: "Sergio Ramos, Frank Lampard", abstract: "How are you doing?", address: "Warsaw, Poland", agreement: true, presenter: "Frank Lampard", abstract_type: "Poster" }
+			post :create, abstract: {user_id: user_session.user, title: "Abstract 2 for Dicty Conference", authors: "Sergio Ramos, Frank Lampard", abstract: "How are you doing?", address: "Warsaw, Poland", agreement: true, presenter: "Frank Lampard", abstract_type: "Poster" }
 		end
 	end
 
 	# TODO - 
 	it "should not accept abstract if agreement not true" do
-		
+
 	end
 
 	# TODO - 
