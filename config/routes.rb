f817bf3647526bdb80187e660fe1914decbc5352
David Jimenez-Morales <biodavidjm@gmail.com>
2015-03-20 22:14:50 -0500

    Final version before announcement
    This is the final version before sending announcement to the mailing
    list

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,26 @@ Dicty11::Application.routes.draw do
 
 	root :to => "home#index", :as => :home
 
-	match '/login' => 'user_sessions#new', :as => :login
-	match '/logout' => 'user_sessions#destroy', :as => :logout
+	# match '/login' => 'user_sessions#new', :as => :login
+	# match '/logout' => 'user_sessions#destroy', :as => :logout
 
-	match '/admin' => 'user_sessions#admin', :as => :admin
+	# match '/admin' => 'user_sessions#admin', :as => :admin
 
 	#DJM match '/register' => 'registration#new', :as => :register 
 	#DJM match '/registrations' => 'registration#view', :as => :registrations
 
-	match '/payment' => 'registration#payment', :as => :payment_redirect
+	# match '/payment' => 'registration#payment', :as => :payment_redirect
 	# match '/users/:id', :to => 'users#show', :as => :user
 
-	resources :user_sessions
-	resources :users
-	resources :registration
-	resources :abstracts
+	# resources :user_sessions
+	# resources :users
+	# resources :registration
 
-	resources :users do
-		resources :abstracts
-	end
+	# resources :abstracts
+
+	# resources :users do
+	# 	resources :abstracts
+	# end
 
 	# resources :users do
 	#   resources :registration
@@ -28,9 +29,11 @@ Dicty11::Application.routes.draw do
 
 	match '/agenda' => 'home#agenda', :as => :agenda
 	match '/transport' => 'home#transport', :as => :transport
-	match '/sponsors' => 'home#sponsors', :as => :sponsors
-	match '/regitration' => 'home#registration', :as => :registration #DJM
-	match '/excursion' => 'home#excursion', :as => :registration #DJM
+	match '/registration' => 'home#registration', :as => :registration #DJM
+	match '/excursion' => 'home#excursion', :as => :excursion #DJM
+	match '/abstracts' => 'home#abstracts', :as => :abstracts #DJM
+	# match '/sponsors' => 'home#sponsors', :as => :sponsors
+
 
 
 end
